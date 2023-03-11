ad8b275dca3f3846fb8dd3330d5f50bef0a29f9e
Abhishek Soni <abhiputulshek@gmail.com>
2023-03-12 00:10:14 +0530

    update page skeleton created

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-update',
   templateUrl: './update.component.html',
   styleUrls: ['./update.component.scss']
 })
-export class UpdateComponent {
+export class UpdateComponent implements OnInit {
 
+  ngOnInit(): void {
+    const  checkAccess =  prompt("Enter password to unlock admin feature")
+  }
 }
